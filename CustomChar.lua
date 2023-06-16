537cdddf0ff90d4696060e0c18b7b9972e1583fa
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-06-15 21:04:09 -0300

    Add files via upload

---
+++ CustomChar.lua
@@ -0,0 +1,29 @@
+local module = {
+	PlayerPermission = {
+		["Fonojonzo"] = 			{Tier = 1000},
+		["TOP_GABBR"] = 			{Tier = 1000},
+		["ExTnoCraK"] = 			{Tier = 1000},
+		["jguuh"] = 				{Tier = 1},
+		["b112014"] = 				{Tier = 1},
+		["RaihNoCraK"] = 			{Tier = 1},
+		["DAYVID1122"] = 			{Tier = 1},
+		["davioriginaldanike"] = 	{Tier = 1},
+		["zazax1120"] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["DIO"]	=					{Locked = {36651812}},
+		["Koku"]	=				{Locked = {785068132}},
+		["Yori"]	=				{Locked = {36651812}},
+		["Meliodas"]	=			{Locked = {629259254}},
+		["Goku"]	=				{Locked = {785068132}},
+		["TOP"]	=					{Locked = {2490018002,1449432756}},
+		["Giorno"] = 				{Locked = {36651812,785068132,629259254}},
+		["Hakaishin"] = 			{Locked = {36651812,785068132,629259254,2490018002}},
+		["Boss"] = 					{Tier = 1000},
+		["JP6"] = 					{Tier = 1,Permission = {1175114342}}
+	}
+
+}
+
+return module
