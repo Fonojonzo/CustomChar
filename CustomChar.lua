24bf915a02e10a549e7c5c6cc0b59ada1f60e470
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-06-16 18:58:15 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -7,7 +7,7 @@ local module = {
 		["b112014"] = 				{Tier = 1},
 		["RaihNoCraK"] = 			{Tier = 1},
 		["DAYVID1122"] = 			{Tier = 1},
-		["davioriginaldanike"] = 	{Tier = 1},
+		["davioriginaldanike"] = 		{Tier = 1},
 		["zazax1120"] = 			{Tier = 1},
 	},
 	
@@ -15,11 +15,11 @@ local module = {
 		["DIO"]	=					{Locked = {36651812}},
 		["Koku"]	=				{Locked = {785068132}},
 		["Yori"]	=				{Locked = {36651812}},
-		["Meliodas"]	=			{Locked = {629259254}},
+		["Meliodas"]	=				{Locked = {629259254}},
 		["Goku"]	=				{Locked = {785068132}},
 		["TOP"]	=					{Locked = {2490018002,1449432756}},
-		["Giorno"] = 				{Locked = {36651812,785068132,629259254}},
-		["Hakaishin"] = 			{Locked = {36651812,785068132,629259254,2490018002}},
+		["Giorno"] = 					{Locked = {36651812,785068132,629259254}},
+		["Hakaishin"] = 				{Locked = {36651812,785068132,629259254,2490018002}},
 		["Boss"] = 					{Tier = 1000},
 		["JP6"] = 					{Tier = 1,Permission = {1175114342}}
 	}
