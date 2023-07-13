b162b5a37e59cb1de6936a12503337d2504b8c35
TopGabBr <129920170+TopGabBr@users.noreply.github.com>
2023-07-13 10:59:49 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -10,7 +10,7 @@ local Tabela = {
 		["davioriginaldanike"] =                {Tier = 1},
 		["zazax1120"] = 			{Tier = 1},
 		["oldvmp"] =				{Tier = 1},
-		["SuperSteel_2"] =                        {Tier = 1},
+		["SuperSteel_2"] =                      {Tier = 1},
 
 	        
 	},
