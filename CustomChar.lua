598f6b45155b5dac7d07ad8753f42f139fee9219
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-07-27 18:08:30 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -1,18 +1,13 @@
 local Tabela = {
 	PlayerPermission = {
 		["Fonojonzo"] = 			{Tier = 1000},
-		["TOP_GABBR"] = 			{Tier = 1000},
 		["ExTnoCraK"] = 			{Tier = 1000},
 		["jguuh"] = 				{Tier = 1},
 		["b112014"] = 				{Tier = 1},
 		["RaihNoCraK"] = 			{Tier = 1},
 		["davioriginaldanike"] =                {Tier = 1},
 		["zazax1120"] = 			{Tier = 1},
-		["oldvmp"] =				{Tier = 1},
-		["SuperSteel_2"] =                      {Tier = 1},
-
-
-	        
+		["SuperSteel_2"] =                      {Tier = 1},       
 	},
 	
 	CharacterTier = {
@@ -23,10 +18,10 @@ local Tabela = {
 		["Yori"]	=				{Locked = {36651812}},
 		["Meliodas"]	=				{Locked = {629259254}},
 		["Goku"]	=				{Locked = {785068132}},
-		["SovietFire"]	=				{Locked = {2490018002}},
-		["TOP"]	=					{Locked = {2490018002,1449432756,143220887}},
+		["SovietFire"]	=				{Locked = {1}},
+		["TOP"]	=					{Locked = {1449432756,143220887}},
 		["Giorno"] = 					{Locked = {36651812,785068132,629259254,13032042}},
-		["Hakaishin"] = 				{Locked = {36651812,785068132,629259254,2490018002,13032042}},
+		["Hakaishin"] = 				{Locked = {36651812,785068132,629259254,13032042}},
 		["Boss"] = 					{Locked = {785068132,36651812}},
 		["JP6"] = 					{Tier = 1,Permission = {1175114342,2908376678,1706565385,3108673472}},
 		["Gojo"] = 					{Tier = 1,Permission = {2929532380,2963030044,1064276494,1706565385,2908376678,3108673472}}, -- perm temp
