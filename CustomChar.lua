8752505d24d853ff1281bea353e6e639cab2d927
iExTno <132175289+iExTno@users.noreply.github.com>
2024-01-21 17:03:47 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -14,7 +14,7 @@ local Tabela = {
 		["Enrico"] = 					{Tier = 1000,Permission = {3108673472}},
 		["DIO"]	=					{Locked = {36651812}},
 		["Madara"]	=				{Locked = {36651812,785068132}},
-		["Koku"]	=				{Locked = {785068132,}},
+		["Koku"]	=				{Locked = {785068132}},
 		["Yori"]	=				{Locked = {36651812}},
 		["Meliodas"]	=				{Locked = {629259254}},
 		["Goku"]	=				{Locked = {785068132}},
