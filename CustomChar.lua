6073f575f2cf4822218bb76937ea51ec2216c01e
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-04-19 21:20:02 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -8,7 +8,7 @@ local Tabela = {
 	},
 	
 	CharacterTier = {
-		["Enrico"] = 					{Tier = 1000,Permission = {3108673472}},
+		["Enrico"] = 					{Tier = 1000,Permission = {3108673472,4035850120}},
 		["DIO"]	=					{Locked = {36651812}},
 		["Madara"]	=				{Locked = {36651812,785068132}},
 		["Koku"]	=				{Locked = {785068132}},
